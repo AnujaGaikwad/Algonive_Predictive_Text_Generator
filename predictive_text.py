c81f41a2129677e608674c81908bca5972c29b62
anuja <anujag2020@gmail.com>
2026-03-12 21:17:39 +0530

    Read dataset from corpus.txt - Applied text cleaning (lowercase + punctuation removal) - Implemented Keras Tokenizer for word indexing - Generated word-to-index dictionary - Computed vocabulary size for model training

--- predictive_text.py
+++ predictive_text.py
@@ -1,17 +1,29 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # hide tensorflow logs
+
 import re
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 # read dataset
 with open("data/corpus.txt", "r", encoding="utf-8") as file:
     text = file.read()
 
-# convert to lowercase
+# cleaning
 text = text.lower()
-
-# remove punctuation
 text = re.sub(r'[^\w\s]', '', text)
 
-# tokenize
-words = text.split()
+print("Dataset Loaded:\n", text)
 
-print("Total words:", len(words))
-print("Sample words:", words[:20])
+# create tokenizer
+tokenizer = Tokenizer()
+
+# fit tokenizer
+tokenizer.fit_on_texts([text])
+
+# word index
+word_index = tokenizer.word_index
+
+print("\nWord Index:")
+print(word_index)
+
+print("\nVocabulary Size:", len(word_index))
